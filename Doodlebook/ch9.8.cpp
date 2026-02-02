d55ff5307d032d083f4b2cefe1807aa95c5c656c
voov-v <yumin5482@gmail.com>
2026-02-02 15:32:42 +0900

    add

---
+++ Doodlebook/ch9.8.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void swap(double& a, double& b) {
+    double tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void swap(char& a, char& b) {
+    char tmp = a;
+    a = b;
+    b = tmp;
+}
+
+int main() {
+    int a = 10, b = 20;
+    double da = 12.34, db = 56.78;
+    char ca = 'A', cb = 'B';
+
+    swap(a, b);
+    swap(da, db);
+    swap(ca, cb);
+
+    cout << a << " " << b << endl;
+    cout << da << " " << db << endl;
+    cout << ca << " " << cb << endl;
+
+}
