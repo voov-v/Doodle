1889222f7a6b9ee016580c1b1e15f95f9b185846
voov <yumin5482@naver.com>
2026-03-29 15:08:12 +0900

    add

---
+++ Doodlebook/ch12.17.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Mom {
+    int a = 1;
+};
+
+struct Dad {
+    int b = 2;
+};
+
+struct Child : Mom, Dad {
+    int c = 3;
+};
+
+int main() {
+    Child ch;
+
+    cout << ch.a << endl;
+    cout << ch.b << endl;
+    cout << ch.c << endl;
+}
---
+++ Doodlebook/ch12.18.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Person {
+    int age;
+    virtual ~Person() {}
+    void Eat() {
+        cout << "먹는다..." << endl;
+    }
+};
+
+struct Student : virtual Person {
+    void Study() {
+        cout << "공부한다..." << endl;
+    }
+};
+
+struct Worker : virtual Person {
+    void Work() {
+        cout << "일한다..." << endl;
+    }
+};
+
+struct Researcher : Student, Worker {
+
+};
+
+int main() {
+    Researcher r;
+    r.age = 20;
+
+    cout << r.age << endl;
+    cout << r.Student::age << endl;
+    cout << r.Worker::age << endl;
+    cout << r.Person::age << endl;
+
+    r.Eat();
+}
